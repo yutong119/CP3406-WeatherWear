32774c231cebda2e1a8d3db1752479d7eb46b252
yutong119 <yutong.ji@my.jcu.edu.au>
2026-06-04 11:53:08 +0800

    Delect repeated import

--- app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/MainActivity.kt
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -21,7 +20,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,7 +34,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
 
 class MainActivity : ComponentActivity() {
